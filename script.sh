b14fdce95347434bd3d053c1c0cdd5bf0c710147
Cristian Bermudez <cbermudez@hg.com.uy>
2018-10-09 18:24:36 -0300

    Adding exit command

--- script.sh
+++ script.sh
@@ -7,7 +7,9 @@ cp -Rf /app/fabric-samples/basic-network/config/* /opt/volumes/orderer-1-persist
 cp -Rf /app/fabric-samples/basic-network/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/* /opt/volumes/orderer-2-persistentvolumeclaim/
 cp -Rf /app/fabric-samples/basic-network/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/* /opt/volumes/orderer-3-persistentvolumeclaim/
 # PEER
-# /opt/volumes/peer-1-persistentvolumeclaim/ NOTHING TO DO
+# /opt/volumes/peer-1-persistentvolumeclaim/ NOTHING TO DO ???
 cp -Rf /app/fabric-samples/basic-network/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp/* /opt/volumes/peer-2-persistentvolumeclaim/
 cp -Rf /app/fabric-samples/basic-network/crypto-config/peerOrganizations/org1.example.com/users/* /opt/volumes/peer-3-persistentvolumeclaim/
 cp -Rf /app/fabric-samples/basic-network/config/* /opt/volumes/peer-4-persistentvolumeclaim/
+
+exit 0
